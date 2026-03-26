264f7a0246f469d8294ff25b59d0cf59574bfa87
Moth042 <2495686390@qq.com>
2026-03-26 19:45:36 +0800

    首次上传

---
+++ 20260326/M.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const int N = 1e5 + 9;
+const int mod = 1e9 + 7;
+const int MOD = 998244353;
+int dx[] = {-1, 0, 1, 0}; // 上右下左
+int dy[] = {0, 1, 0, -1};
+int ddx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int ddy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+// 快读
+inline i128 read()
+{
+    char c = getchar();
+    i128 x = 0, s = 1;
+    while (c < '0' || c > '9')
+    {
+        if (c == '-')
+            s = -1;
+        c = getchar();
+    }
+    return x * s;
+}
+// 快写
+void write(i128 x)
+{
+    if (x > 9)
+        write(x / 10);
+    putchar(x % 10 | 48);
+}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int randint(int l, int r)
+{
+    return uniform_int_distribution{l, r}(rnd);
+}
+void moth()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n + 1), b(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= n; i++)
+        cin >> b[i];
+    auto check = [&](ll x) -> bool
+    {
+        ll mn = -1e18, mx = 1e18;
+        for (int i = 1; i <= n; i++)
+        {
+            ll d = x * b[i];
+            ll l = a[i] - d, r = a[i] + d;
+            mn = max(mn, l);
+            mx = min(mx, r);
+        }
+        if (mn > mx)
+            return 0;
+        return 1;
+    };
+    ll l = 0, r = 1e9;
+    while (l < r)
+    {
+        ll mid = l + r >> 1;
+        if (check(mid))
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    cout << l << '\n';
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    cin >> _;
+    while (_--)
+        moth();
+    return 0;
+}
