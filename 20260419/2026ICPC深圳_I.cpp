e47337f3d7a37ea676befd5851d47bf7de536250
Moth042 <2495686390@qq.com>
2026-04-20 01:01:53 +0800

    今日4题

---
+++ 20260419/2026ICPC深圳_I.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const int N = 1e5 + 9;
+const int mod = 1e9 + 7;
+const int MOD = 998244353;
+int dx[] = {-1, 0, 1, 0}; // 上右下左
+int dy[] = {0, 1, 0, -1};
+int ddx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int ddy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+// 快读
+inline i128 read()
+{
+    char c = getchar();
+    i128 x = 0, s = 1;
+    while (c < '0' || c > '9')
+    {
+        if (c == '-') s = -1;
+        c = getchar();
+    }
+    while (c >= '0' && c <= '9')
+    {
+        x = x * 10 + (c - '0');
+        c = getchar();
+    }
+    return x * s;
+}
+// 快写
+void write(i128 x)
+{
+    if (x < 0)
+    {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 | 48);
+}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int randint(int l, int r)
+{
+    return uniform_int_distribution{l, r}(rnd);
+}
+void moth()
+{
+    string s;
+    cin >> s;
+    if (s == "01")
+    {
+        cout << "Yes\n";
+        cout << "0 0 0 0 0 0 0 0 0 0 0 0\n";
+    }
+    else if (s == "02")
+    {
+        cout << "Yes\n";
+        cout << "0 0 0 0 0 0 0 1 0 0 0 0\n";
+    }
+    else if (s == "03")
+    {
+        cout << "Yes\n";
+        cout << "0 0 0 0 0 0 0 1 2 0 0 0\n";
+    }
+    else if (s == "04")
+    {
+        cout << "Yes\n";
+        cout << "0 0 0 0 0 0 0 1 2 3 0 0\n";
+    }
+    else if (s == "05")
+    {
+        cout << "Yes\n";
+        cout << "0 0 0 0 0 0 0 1 2 3 4 0\n";
+    }
+    else if (s == "06")
+    {
+        cout << "Yes\n";
+        cout << "0 0 0 0 0 0 0 1 2 3 4 5\n";
+    }
+    else if (s == "07")
+    {
+        cout << "Yes\n";
+        cout << "0 6 0 0 0 0 0 1 2 3 4 5\n";
+    }
+    else if (s == "08")
+    {
+        cout << "Yes\n";
+        cout << "0 6 7 0 0 0 0 1 2 3 4 5\n";
+    }
+    else if (s == "11")
+    {
+        cout << "Yes\n";
+        cout << "0 2 3 4 5 6 0 1 7 8 0 0\n";
+    }
+    else if (s == "22")
+    {
+        cout << "Yes\n";
+        cout << "0 1 2 3 4 5 0 1 6 7 8 0\n";
+    }
+    else if (s == "33")
+    {
+        cout << "Yes\n";
+        cout << "0 1 2 3 4 5 0 1 2 6 7 8\n";
+    }
+    else cout << "No\n";
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    cin >> _;
+    while (_--) moth();
+    return 0;
+}
