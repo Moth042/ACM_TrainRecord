31a73887bd1953781e42e990fa0ae9b7219dddca
Moth042 <2495686390@qq.com>
2026-05-16 12:27:29 +0800

    今日3题

---
+++ 20260515/HDU8_E_笑容波状攻击.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const int N = 1e5 + 9;
+const int mod = 1e9 + 7;
+const int MOD = 998244353;
+int dx[] = {-1, 0, 1, 0}; // 上右下左
+int dy[] = {0, 1, 0, -1};
+int ddx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int ddy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+// 快读
+inline i128 read()
+{
+    char c = getchar();
+    i128 x = 0, s = 1;
+    while (c < '0' || c > '9')
+    {
+        if (c == '-') s = -1;
+        c = getchar();
+    }
+    while (c >= '0' && c <= '9')
+    {
+        x = x * 10 + (c - '0');
+        c = getchar();
+    }
+    return x * s;
+}
+// 快写
+void write(i128 x)
+{
+    if (x < 0)
+    {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 | 48);
+}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int randint(int l, int r)
+{
+    return uniform_int_distribution{l, r}(rnd);
+}
+void moth()
+{
+    string s;
+    cin >> s;
+    ll n = s.length();
+    s = " " + s;
+    ll l = 1, r = 1;
+    ll ans = 0, mxlen = n * (n + 1) / 2, mnlen = 0;
+    while (l <= n && r <= n)
+    {
+        while (r <= n && s[r] == s[l]) r++;
+        mnlen += (r - l) * (r - l + 1) / 2;
+        if (s[l] == '0') ans += (r - l) * (r - l + 1) / 2;
+        l = r;
+    }
+    ans += (mxlen - mnlen) * 2;
+    cout << ans << '\n';
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    cin >> _;
+    while (_--) moth();
+    return 0;
+}
