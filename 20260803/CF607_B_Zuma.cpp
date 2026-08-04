bb02bfd418a0432082b9d10f40815048b10df157
Moth042 <2495686390@qq.com>
2026-08-04 17:05:46 +0800

    今日2题

---
+++ 20260803/CF607_B_Zuma.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+const int N = 1e5 + 9;
+const int mod = 1e9 + 7;
+const int MOD = 998244353;
+int dx[] = {-1, 0, 1, 0}; // 上右下左
+int dy[] = {0, 1, 0, -1};
+int ddx[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int ddy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+// 快读
+inline i128 read()
+{
+    char c = getchar();
+    i128 x = 0, s = 1;
+    while (c < '0' || c > '9')
+    {
+        if (c == '-') s = -1;
+        c = getchar();
+    }
+    while (c >= '0' && c <= '9')
+    {
+        x = x * 10 + (c - '0');
+        c = getchar();
+    }
+    return x * s;
+}
+// 快写
+void write(i128 x)
+{
+    if (x < 0)
+    {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 | 48);
+}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int randint(int l, int r)
+{
+    return uniform_int_distribution{l, r}(rnd);
+}
+void moth()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n + 5);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    vector<vector<int>> dp(n + 5, vector<int>(n + 5));
+    for (int len = 1; len <= n; len++)
+    {
+        for (int i = 1, j = len; j <= n; i++, j++)
+        {
+            if (len == 1) dp[i][j] = 1;
+            else
+            {
+                dp[i][j] = 1 + dp[i + 1][j];
+                if (a[i] == a[i + 1]) dp[i][j] = min(dp[i][j], dp[i + 2][j] + 1);
+                for (int k = i + 2; k <= j; k++)
+                {
+                    if (a[i] == a[k]) dp[i][j] = min(dp[i][j], dp[i + 1][k - 1] + dp[k + 1][j]);
+                }
+            }
+        }
+    }
+    cout << dp[1][n] << "\n";
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int _ = 1;
+    // cin >> _;
+    while (_--) moth();
+    return 0;
+}
